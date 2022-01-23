8f18c5fd5c8297a08020b3ce8ca9299b2b02d12a
caichunyu <caichunyu0@gmail.com>
2022-01-23 23:53:42 +0800

    feat: user

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const app = express()
 
 app.set('secret','dghapohgae3423ojo') //token效验
 
+console.log(app.get('secret'),'sssecret')
+
 app.use(require('cors')()) //跨域
 // parse application/x-www-form-urlencoded
 // app.use(express.urlencoded())
