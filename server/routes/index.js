9f420978b5bc41a61091c7bdaffbf07e959d0e76
caichunyu <caichunyu0@gmail.com>
2022-01-25 22:26:00 +0800

    feat: uploads

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,15 @@ module.exports = app => {
   require('./user/user')(app)
   const user = require('./user/user')
   app.use('/api', require('./user/user'))
+
+  //上传部分
+  const multer = require('multer')
+  const upload = multer({dest: __dirname + '/../../uploads'})
+
+  app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res) => {
+    const file = req.file
+    file.url = `http://localhost:3000/uploads/${file.filename}`
+    res.send(file)
+  })
+
 }
